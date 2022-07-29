5050d1ae8a90881014361c797d16a4b17a4a302a
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-07-29 14:06:15 -0300

    searchPage style

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,15 +1,38 @@
+
+import { setMovieCategory } from '../components/setMovieCategory'
 import './SearchPage.css'
 
-
 export const SearchPage = () => {
+
+  // const { data:movies, loading } = useFetchMovies( inputSearch )
+
   return (
     <>
       <div className='background-div'></div>
-      
-      <div className="container">
+
+      <div className="search-container">
+
+        <h3>Search a Movie:</h3>
+
+        <div className="input-container">
+
+          <i className='bx bxs-search-alt-2'></i>
+          <input 
+            type="text"
+
+          />
+
+        </div>
+
+        <div className="movies-container">
+          <ul>
 
 
-        <h1>SearchPage</h1>
+
+          </ul>
+
+        </div>
+
 
       </div>
 
