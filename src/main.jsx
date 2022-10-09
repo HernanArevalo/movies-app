03197fc96e1b741e470f3274a7dd45a7ad4cf7b4
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-10-09 18:55:44 -0300

    docs created

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { store } from './store/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={ store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <MoviesRoutes />
       </BrowserRouter>
     </Provider>
